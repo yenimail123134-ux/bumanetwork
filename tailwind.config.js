a39f50292f8aaaa6615f0ca69450ca61821f3505
yenimail123134-ux <yenimail123134@gmail.com>
2026-02-05 01:32:37 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'buma-black': '#020202',
+        'buma-amber': '#f59e0b',
+      }
+    },
+  },
+  plugins: [],
+}
